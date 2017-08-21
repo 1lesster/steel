56793df481101b5e82383df2bdd256c6cc7e38d6
alexp <teplo.alexey@gmail.com>
2017-08-21 17:23:22 +0300

    123

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$( document ).ready(function() {
+    $('.start-slider-wrap').slick({
+    	draggable: false,
+    	appendArrows: $('.control'),
+    	nextArrow: '<i class="slick-arrow demo-icon icon-arrow next"></i>',
+    	prevArrow: '<i class="slick-arrow demo-icon icon-arrow prev"></i>'
+    });
+    $('.hot-offers .sidebar-slider').slick({
+    	draggable: false,
+    	nextArrow: '<i class="slick-arrow demo-icon icon-arrow next"></i>',
+    	prevArrow: '<i class="slick-arrow demo-icon icon-arrow prev"></i>'
+    });
+    $('.reviews .sidebar-slider').slick({
+    	draggable: false,
+    	nextArrow: '<i class="slick-arrow demo-icon icon-arrow next"></i>',
+    	prevArrow: '<i class="slick-arrow demo-icon icon-arrow prev"></i>'
+    });
+
+    $(".catalog-nav a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".catalog-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+
+    var navHeight = $('.catalog-nav').height();
+		$('.catalog-content').css("height", navHeight);
+
+});
